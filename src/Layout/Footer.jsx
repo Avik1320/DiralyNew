5bb9ea7a2ef8de8bf755ceaf34bf560fa0a965ce
Avik Ghosh <98302991+Avik1320@users.noreply.github.com>
2023-12-20 12:17:47 +0530

    first commit maintaince page

---
+++ src/Layout/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer-main'>
+      <div className="footer-main-part">
+        <div className="language">English</div>
+        <div className="money">Euro</div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Pages/CommingSoon.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { FaTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import emailjs from "@emailjs/browser";
+
+const CommingSoon = () => {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // API FOR THE MAIL SENDING
+    const serviceId = "service_jxcs1rs";
+    const templateId = "template_cva25qx";
+    const publicKey = "Lnk6Rg3UdS8PK-h3z";
+
+    // api params
+    const TemplateParams = {
+      from_email: email,
+      to_name: "office@diraly.com",
+    };
+
+    // send e-mail
+    emailjs
+      .send(serviceId, templateId, TemplateParams, publicKey)
+      .then((Response) => {
+        console.log("E-mail sent bro ", Response);
+
+        setEmail("");
+      })
+      .catch((error) => {
+        console.error("Error in mail sending ", error);
+      });
+  };
+
+  return (
+    <div className="commingsoon-main">
+      <div className="subhead-1">diraly</div>
+      <div className="subhead-2">home, anywhere</div>
+      <div className="subhead-3">Comming Soon</div>
+      <div className="subhead-4">Be the first to know when we go live</div>
+      <div className="subhead-input">
+        <form onSubmit={handleSubmit} className="input-form">
+          <input
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="inp-email"
+          />
+          <button className="submit">Submit</button>
+        </form>
+      </div>
+      <div className="social-media">
+        <div className="twitter">
+          <FaTwitter />
+        </div>
+        <div className="instagram">
+          <FaInstagram />
+        </div>
+        <div className="facebook">
+          <FaFacebook />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CommingSoon;
